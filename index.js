15640aaa0c21fb00b5e6bc928c72210501ae69dc
amchos <124582071+amchos@users.noreply.github.com>
2025-09-24 12:15:16 +0400

    first version

--- index.js
+++ index.js
@@ -27,3 +27,49 @@ const posts = [
         likes: 152
     }
 ]
+
+const postEl = document.querySelector('.post-wrapper')
+
+
+
+console.log(posts.length)
+
+function postRender(post, toWhere) {
+    for(let i = 0; i < post.length; i++) {
+        toWhere.innerHTML += `            
+                <div class="post">
+                    <div class="post-user">
+                        <div class="post-user-img">
+                            <img src="${post[i].avatar}" alt="profile picture of post owner">
+                        </div>
+                        <div class="post-user-info">
+                            <div class="post-user-info-name">${post[i].username}</div>
+                            <div class="post-user-info-town">${post[i].location}</div>
+                        </div>
+                    </div>
+                    <div class="post-img">
+                        <img src="${post[i].post}" alt="Post image: Vangogh">
+                    </div>
+                    <div class="post-bottom">
+                        <div class="post-bottom-btn">
+                            <button>
+                                <img src="images/icon-heart.png" alt="like button image">
+                            </button>
+
+                            <button>
+                                <img src="images/icon-comment.png" alt="comment button image">
+                            </button>
+
+                            <button>
+                                <img src="images/icon-dm.png" alt="share button image">
+                            </button>
+                        </div>
+                        <div class="post-bottom-likes-amount"><strong>${post[i].likes}</strong> likes</div>
+                        <div class="post-bottom-comment"><strong>${post[i].username}</strong> ${post[i].comment}</div>
+                    </div>
+                </div>`
+    }
+}
+
+
+postRender(posts, postEl);
